f7822b7bdc56e7ed8783a2b35eaef701e6077d5b
michal <micdeg96@gmail.com>
2020-11-17 20:58:46 +0100

    Zadanie 1, 2 i menu

---
+++ zad2.py
@@ -0,0 +1,37 @@
+from math import e
+
+"""
+What is this system's reliability at 700 hours?
+Component failure data is :
+Failure rate of A = 0.0007 failures/hr
+Reliability of B = 0.92
+MTTF of C = 1400 hours
+Reliability of D = 0.85
+
+    --------- A ---------
+    |                   |
+    |                   |
+    |                   |
+    --------- B ---------
+    |                   |
+----|                   |----
+    |                   |
+    --------- C ---------
+    |                   |
+    |                   |
+    |                   |
+    --------- D ---------
+"""
+
+
+def find_system_reliability():
+    time = input("Podaj czas: ")
+    failure_rate_A = input("Podaj wskaźnik awaryjności (Failure rate): ")
+    reliability_B = input("Podaj niezawodność (Reliability): ")
+    MTTF_C = input("Podaj średni czas do wystąpienia awarii (MTTF): ")
+    reliability_D = input("Podaj niezawodność (Reliability): ")
+    reliability_A = (e ** (-(float(failure_rate_A) * int(time))))
+    reliability_C = (e ** (-(int(time) / int(MTTF_C))))
+    reliability_S = (1 - (1 - float(reliability_A)) * (1 - float(reliability_B)) * (1 - float(reliability_C))
+                     * (1 - float(reliability_D)))
+    return print("System reliability: ", round(reliability_S, 3))
